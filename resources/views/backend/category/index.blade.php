c7734a8c756004122644ca36d842537ebc77d1d0
Rippete <pooripatwaitchasit@gmail.com>
2024-01-29 14:38:18 +0700

    first commit

---
+++ resources/views/backend/category/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master_backend')
+@section('contant')
+<div class="content-wrapper">
+  <!-- Content -->
+
+  <div class="container-xxl flex-grow-1 container-p-y">
+    <div class="card">
+      <h5 class="card-header">Category</h5>
+      <div class="table-responsive text-nowrap">
+        <a href="{{ url('admin/category/createfrom') }}" class="btn btn-success mx-3"><i class='bx bxs-plus-circle'></i> เพิ่มข้อมูล</a>
+        <table class="table mt-4">
+          <thead class="table-dark">
+            <tr>
+              <th>category_id</th>
+              <th>name</th>
+              <th>created_at</th>
+              <th>updated_at</th>
+            </tr>
+          </thead>
+          <tbody class="table-border-bottom-0">
+            @foreach ($category as $cat)
+            <tr>
+              <td>{{ $category->firstItem() + $loop->index }}</td>
+              <td>{{ $cat->name }}</td>
+              <td>{{ $cat->created_at }}</td>
+              <td>{{ $cat->updated_at }}</td>
+              <td>
+                <a href="{{ url('admin/category/edit/'.$cat->category_id) }}"><i class='bx bxs-edit'></i></a>
+                <a href="{{ url('admin/category/delete/'.$cat->category_id) }}"><i class='bx bx-trash'></i></a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+        <div class="mt-3 container">
+          {{ $category->links('pagination::bootstrap-5') }}
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+@endsection
